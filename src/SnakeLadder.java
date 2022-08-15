59f60dfe597624d7ae4b9ef55727264da52be16f
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-16 00:13:42 +0530

    Added: Get Die Value After Rolling The Die

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -4,7 +4,15 @@ public class SnakeLadder {
     public static int player = 1;
     public static int winPoints = 100;
 
+    public static int rollingDie(){
+        int rollValue = (int)Math.floor(((Math.random() * 10)%6+1));
+        return rollValue;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder Program!!!");
+
+        int rollValue = rollingDie();
+        System.out.println("Roll Value after rolling is: "+rollValue);
     }
 }
